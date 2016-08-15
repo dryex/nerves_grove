8cd905a9c46124e9937a3a83fce5227156b8193b
Arto Bendiken <arto@bendiken.net>
2016-08-15 23:47:23 +0200

    Added placeholders for Grove submodules.

---
+++ lib/nerves_grove/i2c.ex
@@ -0,0 +1,5 @@
+# This is free and unencumbered software released into the public domain.
+
+defmodule Nerves.Grove.I2C do
+  # TODO
+end
---
+++ lib/nerves_grove/sensor.ex
@@ -0,0 +1,5 @@
+# This is free and unencumbered software released into the public domain.
+
+defmodule Nerves.Grove.Sensor do
+  # TODO
+end
