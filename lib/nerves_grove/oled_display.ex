752c19acde6014f348a9869c83b24d6aff590d07
Arto Bendiken <arto@bendiken.net>
2016-08-16 02:19:16 +0200

    Implemented OLED display commands from the SSD1327 datasheet.

--- lib/nerves_grove/oled_display.ex
+++ lib/nerves_grove/oled_display.ex
@@ -3,6 +3,7 @@
 defmodule Nerves.Grove.OLED.Display do
   @moduledoc """
   http://wiki.seeedstudio.com/wiki/Grove_-_OLED_Display_1.12%22
+  http://garden.seeedstudio.com/images/8/82/SSD1327_datasheet.pdf
   """
 
   @default_address 0x3C
@@ -16,19 +17,42 @@ defmodule Nerves.Grove.OLED.Display do
 
   @spec reset(pid) :: :ok
   def reset(pid) do
-    send_commands(pid,
-      <<0xFD, 0x12, 0xAE, 0xA8, 0x5F, 0xA1, 0x00, 0xA2,
-        0x60, 0xA0, 0x46, 0xAB, 0x01, 0x81, 0x53, 0xB1,
-        0x51, 0xB3, 0x01, 0xB9, 0xBC, 0x08, 0xBE, 0x07,
-        0xB6, 0x01, 0xD5, 0x62, 0xA4, 0x2E, 0xAF>>)
+    send_commands(pid, <<0xFD, 0x12>>)
+    off(pid)
+    set_multiplex_ratio(pid, 95) # 96
+    set_start_line(pid, 0)
+    set_display_offset(pid, 96)
+    set_vertical_mode(pid)
+    send_commands(pid, <<0xAB, 0x01>>)
+    set_contrast_level(pid, 0x53) # 100 nit
+    send_commands(pid, <<0xB1, 0x51>>)
+    send_commands(pid, <<0xB3, 0x01>>)
+    send_commands(pid, <<0xB9>>)
+    send_commands(pid, <<0xBC, 0x08>>)
+    send_commands(pid, <<0xBE, 0x07>>)
+    send_commands(pid, <<0xB6, 0x01>>)
+    send_commands(pid, <<0xD5, 0x62>>)
+    set_normal_mode(pid)
+    set_activate_scroll(pid, false)
+    on(pid)
     :timer.sleep(100) # ms
     set_row_address(pid, 0, 95)
     set_column_address(pid, 8, 8 + 47)
   end
 
-  @spec set_normal(pid) :: :ok
-  def set_normal(pid) do
-    send_command(pid, 0xA4)
+  @spec on(pid) :: :ok
+  def on(pid) do
+    send_command(pid, 0xAF)
+  end
+
+  @spec off(pid) :: :ok
+  def off(pid) do
+    send_command(pid, 0xAE)
+  end
+
+  @spec set_column_address(pid, byte, byte) :: :ok
+  def set_column_address(pid, start, end_) do
+    send_commands(pid, <<0x15, start, end_>>)
   end
 
   @spec set_row_address(pid, byte, byte) :: :ok
@@ -36,9 +60,56 @@ defmodule Nerves.Grove.OLED.Display do
     send_commands(pid, <<0x75, start, end_>>)
   end
 
-  @spec set_column_address(pid, byte, byte) :: :ok
-  def set_column_address(pid, start, end_) do
-    send_commands(pid, <<0x15, start, end_>>)
+  @spec set_contrast_level(pid, byte) :: :ok
+  def set_contrast_level(pid, level) do
+    send_commands(pid, <<0x81, level>>)
+  end
+
+  @spec set_horizontal_mode(pid) :: :ok
+  def set_horizontal_mode(pid) do
+    send_commands(pid, <<0xA0, 0x42>>)
+    set_row_address(pid, 0, 95)
+    set_column_address(pid, 8, 8 + 47)
+  end
+
+  @spec set_vertical_mode(pid) :: :ok
+  def set_vertical_mode(pid) do
+    send_commands(pid, <<0xA0, 0x46>>)
+  end
+
+  @spec set_start_line(pid, 0..127) :: :ok
+  def set_start_line(pid, row) do
+    send_commands(pid, <<0xA1, row>>)
+  end
+
+  @spec set_display_offset(pid, 0..127) :: :ok
+  def set_display_offset(pid, row) do
+    send_commands(pid, <<0xA2, row>>)
+  end
+
+  @spec set_normal_mode(pid) :: :ok
+  def set_normal_mode(pid) do
+    send_command(pid, 0xA4)
+  end
+
+  @spec set_inverse_mode(pid) :: :ok
+  def set_inverse_mode(pid) do
+    send_command(pid, 0xA7)
+  end
+
+  @spec set_multiplex_ratio(pid, 16..128) :: :ok
+  def set_multiplex_ratio(pid, ratio) do
+    send_commands(pid, <<0xA8, ratio>>)
+  end
+
+  @spec set_activate_scroll(pid, false) :: :ok
+  def set_activate_scroll(pid, false) do
+    send_command(pid, 0x2E)
+  end
+
+  @spec set_activate_scroll(pid, true) :: :ok
+  def set_activate_scroll(pid, true) do
+    send_command(pid, 0x2F)
   end
 
   @spec clear(pid, byte) :: :ok
